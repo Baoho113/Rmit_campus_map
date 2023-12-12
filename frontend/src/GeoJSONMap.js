da92113a904bb0143338bcd2a8eab531627ec513
huchuu <s3891958@rmit.edu.vn>
2023-12-12 19:59:02 +0700

    GPS applied, Displays current location in real time

--- frontend/src/GeoJSONMap.js
+++ frontend/src/GeoJSONMap.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import geojsonFeature from "./building_info/buildingCollection";
@@ -6,17 +6,47 @@ import univLayer from "./univLayer.json";
 
 const GeoJSONMap = () => {
   const [selectedBuilding, setSelectedBuilding] = useState(null);
+  const [userLocation, setUserLocation] = useState(null);
+  const mapRef = useRef(); // Ref to store the map object
 
   useEffect(() => {
-    const map = L.map("map", {
+    // Create a map and store it in the ref
+    mapRef.current = L.map("map", {
       center: [10.7298, 106.69451],
       zoom: 20,
     });
 
+    // Add OpenStreetMap tile layer
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "© OpenStreetMap contributors",
-    }).addTo(map);
+    }).addTo(mapRef.current);
 
+    // Function to handle user's location
+    const handleLocation = (location) => {
+      const { latitude, longitude } = location.coords;
+      setUserLocation([latitude, longitude]);
+
+      // Add a marker for the user's location
+      const userMarker = L.marker([latitude, longitude], {
+        icon: L.icon({
+          iconUrl: "/images/person.png", 
+          iconSize: [32, 32],
+          iconAnchor: [16, 32],
+          popupAnchor: [0, -32],
+        }),
+      }).addTo(mapRef.current);
+
+      userMarker.bindPopup("Your Location").openPopup();
+    };
+
+    // Get user's location using Geolocation API
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(handleLocation);
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+
+    // Add university layer
     const univLayerGroup = L.geoJSON(univLayer, {
       style: {
         fillColor: "yellow",
@@ -32,8 +62,9 @@ const GeoJSONMap = () => {
       },
     });
 
-    univLayerGroup.addTo(map);
+    univLayerGroup.addTo(mapRef.current);
 
+    // Add building polygons
     const buildingPolygons = [];
     const layers = L.geoJSON(geojsonFeature, {
       style: {
@@ -52,9 +83,10 @@ const GeoJSONMap = () => {
       },
     });
 
-    layers.addTo(map);
+    layers.addTo(mapRef.current);
 
-    map.on("click", function (e) {
+    // Handle click on the map
+    mapRef.current.on("click", function (e) {
       const clickedLatLng = e.latlng;
       const clickedBuilding = buildingPolygons.find((layer) =>
         layer.getBounds().contains(clickedLatLng)
@@ -76,24 +108,29 @@ const GeoJSONMap = () => {
 
           L.polyline([sourceLatLng, targetLatLng], {
             color: "red",
-          }).addTo(map);
+          }).addTo(mapRef.current);
         }
       });
     });
 
     return () => {
-      map.remove();
+      mapRef.current.remove();
     };
   }, []);
 
   return (
     <div>
       <div id="map" style={{ height: "500px" }}></div>
+      {userLocation && (
+        <div className="navigation-bar">
+          <p>Your Location: {userLocation[0]}, {userLocation[1]}</p>
+        </div>
+      )}
       {selectedBuilding && (
         <div className="navigation-bar">
           <button onClick={() => setSelectedBuilding(null)}>X</button>
           <h2>{selectedBuilding}</h2>
-          <img src={`images/${selectedBuilding}.jpg`} alt={selectedBuilding} />
+          <img src={`images/selectedBuilding.jpg`} alt={selectedBuilding} />
           <p>Description of {selectedBuilding}</p>
         </div>
       )}
